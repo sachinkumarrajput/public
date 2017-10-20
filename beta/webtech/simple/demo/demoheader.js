e4062c33a70721ee53b35e7bdcaa20d3101ce51f
skr571999 <31756244+skr571999@users.noreply.github.com>
2017-10-20 09:27:23 +0530

    Initial commit

---
+++ beta/webtech/simple/demo/demoheader.js
@@ -0,0 +1,6 @@
+document.write("<header><h1>Demo Header");
+document.write("<i class='closebtn material-icons'>clear</i>");
+document.write("</h1></header>");
+document.write("<footer><div><p>copyright 2017 webstart</p>");
+document.write("</div></footer>");
+document.getElementsById('demo').style="background-color: red;";
